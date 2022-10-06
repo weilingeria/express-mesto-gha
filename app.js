213ea7d1906702bfe52be9651ec1408d78cd1da6
weilingeria <valeria.natsibulina@yandex.ru>
2022-10-06 18:10:43 +0300

    ПР13.1

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Запуск на 3000 порту
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
-// Подключение к серверу MongoDB
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   req.user = {
     _id: '633c900f73c4712dfb593921',
   };
-
   next();
 });
 
-module.exports.createCard = (req, res) => {
-  console.log(req.user._id);
-};
+// Подключение к серверу MongoDB
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('/*', (req, res) => {
+  res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT);
